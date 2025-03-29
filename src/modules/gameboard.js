be83d88f12df1df956be0326dea6313b2c1b5e35
Nathan Y <193931003+nyamashiro@users.noreply.github.com>
2025-03-29 13:31:51 -0400

    tracks hits, misses, sunk ships and game over

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -3,9 +3,11 @@ import { gameboardHelperFunctions } from "./gameboard-helpers.js";
 import { shipHelperFunctions } from "./ship-helpers.js";
 
 class Gameboard {
-  constructor(missed_shots = [], gameOver = false) {
-    this.missed_shots = missed_shots;
+  constructor(missedShots = [], gameOver = false, hits = [], sunkShips = []) {
+    this.missedShots = missedShots;
     this.gameOver = gameOver;
+    this.hits = hits;
+    this.sunkShips = sunkShips;
   }
 
   placeShips() {
@@ -26,5 +28,51 @@ class Gameboard {
     }
     shipHelperFunctions.addShipId(ships);
     this.ships = ships;
+  }
+
+  receiveAttack(coordinates) {
+    let concatHitSet = shipHelperFunctions.concatHitSet(this.hits);
+
+    let missedShotsSet = new Set(
+      this.missedShots.map((shots) => shots.toString())
+    );
+
+    if (missedShotsSet.has(coordinates.toString())) {
+      //should run receiveAttack once more but with different coordinates
+      // console.log("OOPS THIS SHOT IS A MISSED SHOT");
+      return;
+    }
+
+    if (concatHitSet.has(coordinates.toString())) {
+      //should run receiveAttack once more but with different coordinates
+      // console.log("OOPS THIS SHOT ALREADY HIT");
+      return;
+    }
+
+    let hit = false;
+    for (let i = 0; i < this.ships.length; i++) {
+      if (this.ships[i].placementSet.has(coordinates.toString())) {
+        this.ships[i].numOfHits += 1;
+        this.hits.push(coordinates);
+        this.ships[i].isSunk();
+        if (this.ships[i].sunk) {
+          this.sunkShips.push(this.ships[i].id);
+          this.checkGameOver();
+        }
+        hit = true;
+        return;
+      }
+    }
+    if (!hit) {
+      this.missedShots.push(coordinates);
+    }
+  }
+
+  checkGameOver() {
+    if (this.sunkShips.length === this.ships.length) {
+      this.gameOver = true;
+      console.log("GAME OVER!!!!");
+      return;
+    }
   }
 }
